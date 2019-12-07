c84a112868f5332002008d43837f629f865c96e1
gzavaglia <gio.zavaglia@gmail.com>
2019-12-07 14:45:35 -0600

    set up controller

--- back-end/wires-api/app/controllers/api/v1/wires_controller.rb
+++ back-end/wires-api/app/controllers/api/v1/wires_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::WiresController < ApplicationController
+    def index
+        @wires = Wire.all
+
+        render json: @wires, status: 200
+    end
+
+    def show
+        @wire = Wire.find(params[:id])
+        
+        render json: @wire, status: 200
+    end
+
+    def create
+        @wire = Wire.create(wire_params)
+
+        render json: @wire, status: 200
+    end
+
+    def update
+        @wire = Wire.find(params[:id])
+        @wire.update(wire_params)
+        render json: @wire, status: 200
+    end
+
+    def destroy
+        @wire = Wire.find(params[:id])
+        @wire.delete
+        render json: {wireId: @wire.id}
+    end
+
+    private
+
+    def wire_params
+        params.require(:wire).permit(:amps, :volts, :sizing)
+    end
+
+
 end
