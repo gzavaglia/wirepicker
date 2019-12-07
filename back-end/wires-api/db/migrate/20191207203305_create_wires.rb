3a7c1791cac8047ca43e12778026b31b8400e834
gzavaglia <gio.zavaglia@gmail.com>
2019-12-07 14:50:51 -0600

    migration created

--- back-end/wires-api/db/migrate/20191207203305_create_wires.rb
+++ back-end/wires-api/db/migrate/20191207203305_create_wires.rb
@@ -1,7 +1,7 @@
 class CreateWires < ActiveRecord::Migration[5.2]
   def change
     create_table :wires do |t|
-      t.interger :amps
+      t.integer :amps
       t.integer :volts
       t.string :sizing
 
