1a279c7811c76e21e3c10708b7d716d6aced9e3f
gzavaglia <gio.zavaglia@gmail.com>
2019-12-07 14:48:30 -0600

    namespaced routes

--- back-end/wires-api/config/routes.rb
+++ back-end/wires-api/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :wires
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :wires
+    end
+  end
 end
