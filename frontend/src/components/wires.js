5a8b1d0d4d60eb347fca4f3eb3a3311d11db313b
gzavaglia <gio.zavaglia@gmail.com>
2019-12-15 13:11:01 -0600

    modified form and started working on my wires class

--- frontend/src/components/wires.js
+++ frontend/src/components/wires.js
@@ -3,5 +3,12 @@ class Wires{
         this.wires = []
         this.adapter = new WiresAdapter()
         this.bindEventListeners()
+        this.fetchAndLoadWires()
+    }
+
+    fetchAndLoadWires(){
+        this.adapter.getWires().then(wires => {
+            console.log(wires)
+        })
     }
 }
